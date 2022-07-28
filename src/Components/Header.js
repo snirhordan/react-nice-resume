ac183dd4a3865eec3e740fe98ad59b0baf56735c
snirhordan <102667060+snirhordan@users.noreply.github.com>
2022-07-28 20:46:43 +0300

    procfie

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -42,17 +42,6 @@ class Header extends Component {
               </a>
             </li>
 
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Works
-              </a>
-            </li>
-
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
           </ul>
         </nav>
 
